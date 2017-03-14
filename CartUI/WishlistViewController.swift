96e5b4932a5c97de5231a2d1e1cd2d6245af0bab
jakkawad <sunnysunoc@gmail.com>
2017-03-14 16:50:45 +0700

    copy CartUI to WishlistUI

---
+++ CartUI/WishlistViewController.swift
@@ -0,0 +1,50 @@
+//
+//  WishlistViewController.swift
+//  CartUI
+//
+//  Created by admin on 3/14/2560 BE.
+//  Copyright © 2560 Jakkawad.Chaiplee. All rights reserved.
+//
+
+import UIKit
+
+class WishlistViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell0 = tableView.dequeueReusableCell(withIdentifier: "tableCell0")
+        return cell0!
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
